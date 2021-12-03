edf060eb6f2a9fe37c1585c14a1b82fdf92f35e4
BartoszKosmider <bartekkosmider123@gmail.com>
2021-12-03 22:08:20 +0100

    fin

--- rsa/Program.cs
+++ rsa/Program.cs
@@ -13,104 +13,110 @@ namespace rsa
 		{
 			//BigInteger p = 31;
 			//BigInteger q = 19;
-			//BigInteger p = 1583;
-			//BigInteger q = 1609;
-			BigInteger p = 1823;
-			BigInteger q = 1879;
-			var n = p * q;
-			var fiN = (p - 1) * (q - 1);
+			BigInteger p = 25939;
+			BigInteger q = 26557;
+			//BigInteger p = 1823;
+			//BigInteger q = 1879;
+			//BigInteger p = 3671;
+			//BigInteger q = 3673;
+			BigInteger n = p * q;
+			BigInteger fiN = (p - 1) * (q - 1);
 
-			var collectionOfE = GenerateEnumbers(n, fiN).ToList();
-			var random = new Random();
-			//var e = collectionOfE[random.Next(0, collectionOfE.Count - 1)];
-
-			var dList = new List<int>();
-			var eList = new List<int>(collectionOfE);
-
-			foreach (var ee in collectionOfE)
+			Console.WriteLine("type: 'e' to encrypt with public key, 'd' to encrypt with private key ");
+			var option = "";
+			while (true)
 			{
-				bool found = false;
-				for (int dd = 3; dd < fiN; dd++)
-				{
-					//Console.WriteLine($"d: {d}, e: {e}, de: {d*e} mod: {d*e%fiN} ");
-					if (ee * dd % fiN == 1)
-					{
-						dList.Add(dd);
-						found = true;
-						break;
-					}
-				}
-				if (!found)
-					eList.Remove(ee);
+				option = Console.ReadLine();
+				if (option == "e" || option == "d")
+					break;
+				Console.WriteLine("invalid data provided");
 			}
 
-			//var collectionOfD = GenerateDnumbers(fiN, collectionOfE).ToList();
-			var min = n;
-			int d = 0;
-			int e = 0;
-			for (int i = 0; i < dList.Count; i++)
+			var collectionOfE = GenerateEandD(n, fiN);
+			BigInteger encryptKey, decryptKey;
+				;
+
+			if (option == "e")
 			{
-				if (dList[i] < min)
-				{
-					min = dList[i];
-					d = dList[i];
-					e = eList[i];
-				}
+				encryptKey = collectionOfE.LastOrDefault().Key;
+				decryptKey = collectionOfE.LastOrDefault().Value;
 			}
-			Console.WriteLine($"input message to encrypt, p: {p}, q: {q}, e: {e}, d: {d}");
-			//var input = Console.ReadLine();
+			else
+			{
+				encryptKey = collectionOfE.LastOrDefault().Value;
+				decryptKey = collectionOfE.LastOrDefault().Key;
+			}
 
-			//var input = File.ReadAllText("input30.txt");
-			var input = File.ReadAllText("input50.txt");
 
 
-			var encryptedMessage = EncryptMessage(input, e, n);
+			Console.WriteLine($"input message to encrypt, p: {p}, q: {q}, e: {encryptKey}, d: {decryptKey}");
+			var input = File.ReadAllText(fileName20Length);
+			//var input = File.ReadAllText(fileName50Length);
+			var encryptedMessage = EncryptMessage(input, decryptKey, n);
+			
+			File.Delete(decrypted);
+			File.Delete(encrypted);
+			foreach (var singleLetter in encryptedMessage)
+				File.AppendAllText(encrypted, $"{singleLetter} ");
 
-			//foreach (var singleLetter in encryptedMessage)
-			//	Console.Write($"{singleLetter} ");
-
-			var decryptedMessage = DecryptMessage(encryptedMessage, d, n);
+			var decryptedMessage = DecryptMessage(encryptedMessage, encryptKey, n);
+			File.WriteAllText(decrypted, decryptedMessage);
 			Console.WriteLine($"\noutput: {decryptedMessage}");
 		}
 
-		//private static IEnumerable<int> GenerateDnumbers(BigInteger fiN, List<int> eList)
-		//{
-		//	var tempList = new List<int>();
-		//	var tempeList = new List<int>();
-			
-		//	foreach (var e in eList)
-		//	{
-		//		bool found = false;
-		//		for (int d = 3; d < fiN; d++)
-		//		{
-		//			//Console.WriteLine($"d: {d}, e: {e}, de: {d*e} mod: {d*e%fiN} ");
-		//			if (e * d % fiN == 1)
-		//			{
-		//				tempList.Add(d);
-		//				found = true;
-		//				continue;
-		//			}
-		//			if (!found)
-		//				tempeList.Remove(e);
-		//		}
-		//	}
-		//}
-
-
-		private static IEnumerable<int> GenerateEnumbers(BigInteger n, BigInteger fiN)
+		public static BigInteger nwd(BigInteger a, BigInteger b)
 		{
-			for (int i = 3; i < Math.Sqrt((double) n); i++)
+			BigInteger x = a, y = b;
+			while (x != y)
 			{
-				if (IsPrime(i) && fiN % i != 0)
-					yield return i;
+				if (x > y)
+					x = x - y;
+				else
+					y = y - x;
 			}
+			return x;
 		}
 
-		private static bool IsPrime(int number)
+		private static Dictionary<BigInteger, BigInteger> GenerateEandD(BigInteger n, BigInteger fiN)
 		{
-			bool CalculatePrime(int value)
+			var result = new Dictionary<BigInteger, BigInteger>();
+			int j = 0;
+			for (var i = 3; i < 100; i= i+2)
 			{
-				var possibleFactors = Math.Sqrt(number);
+				if (IsPrime(i) && nwd(i, fiN) == 1)
+				{
+					var d = GenerateDNumber(i, fiN);
+					if (d != null)
+					{
+						result.Add(i, (BigInteger)d);
+						j++;
+					}
+				}
+				if(j == 2)
+					break;
+			}
+
+			return result;
+		}
+
+		private static int? GenerateDNumber(int e, BigInteger fiN)
+		{
+			for (var dd = 3; dd < fiN; dd++)
+			{
+				if (e * dd % fiN == 1)
+				{
+					return dd;
+				}
+			}
+
+			return null;
+		}
+
+		private static bool IsPrime(BigInteger number)
+		{
+			bool CalculatePrime(BigInteger value)
+			{
+				var possibleFactors = Math.Sqrt((double)number);
 				for (var factor = 2; factor <= possibleFactors; factor++)
 				{
 					if (value % factor == 0)
@@ -123,23 +129,61 @@ namespace rsa
 			return number > 1 && CalculatePrime(number);
 		}
 
-		private static IEnumerable<BigInteger> EncryptMessage(string input, int e, BigInteger n)
+		private static IEnumerable<BigInteger> EncryptMessage(string input, BigInteger e, BigInteger n)
 		{
 			foreach (var singleByte in Encoding.ASCII.GetBytes(input))
-				yield return BigInteger.Pow(singleByte, e) % n;
+			{
+				BigInteger wynik = 1;
+				var x = singleByte % n;
+				var byteValue = Convert.ToString((int)e, 2);
+				for (int j = byteValue.Length - 1; j >= 0; j--)
+				{
+					if (byteValue[j] == '0')
+					{
+						x = x * x % n;
+					}
+					else
+					{
+						wynik = wynik * x % n;
+						x = x * x % n;
+					}
+				}
+				yield return wynik;
+			}
 		}
 
-		private static string DecryptMessage(IEnumerable<BigInteger> encryptedMessage, int d, BigInteger n)
+		private static string DecryptMessage(IEnumerable<BigInteger> encryptedMessage, BigInteger d, BigInteger n)
 		{
 			var decryptedMessage = "";
 			int i = 0;
 			foreach (var singleLetter in encryptedMessage)
 			{
-				Console.WriteLine($"i: {i++}, encMessLen: {encryptedMessage.Count()}");
-				var decryptedLetter = BigInteger.Pow(singleLetter, d) % n;
-				decryptedMessage += (char) decryptedLetter;
+				BigInteger wynik = 1;
+				var x = singleLetter % n;
+				var byteValue = Convert.ToString((int)d, 2);
+				for (int j = byteValue.Length - 1; j >= 0; j--)
+				{
+					if (byteValue[j] == '0')
+					{
+						x = x * x % n;
+					}
+					else
+					{
+						wynik = wynik * x % n;
+						x = x * x% n;
+					}
+				}
+
+				//Console.WriteLine($"i: {i++}, sing: {singleLetter}, dec: {decryptedLetter}");
+				decryptedMessage += (char) wynik;
 			}
 			return decryptedMessage;
 		}
+
+		private static string fileName20Length = "../../../input20.txt";
+		private static string fileName50Length = "../../../input50.txt";
+		private static string encrypted = "../../../encrypted.txt";
+		private static string decrypted = "../../../decrypted.txt";
+
 	}
 }
